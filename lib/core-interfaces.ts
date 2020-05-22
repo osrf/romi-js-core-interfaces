38e8162e4dbd7a820d06b08dcb7bfb1e5720b1c6
koonpeng <koonpeng@openrobotics.org>
2020-05-22 08:57:00 +0800

    feat: add a bunch of topic mappings

--- lib/core-interfaces.ts
+++ lib/core-interfaces.ts
@@ -16,6 +16,12 @@ export const doorRequests: RomiTopic<msgs.DoorRequest> = {
   topic: 'door_requests',
 };
 
+export const adapterDoorRequests: RomiTopic<msgs.DoorRequest> = {
+  validate: msg => msgs.DoorRequest.fromObject(msg),
+  type: msgs.DoorRequest.typeName,
+  topic: 'adapter_door_requests',
+};
+
 export const liftStates: RomiTopic<msgs.LiftState> = {
   validate: msg => msgs.LiftState.fromObject(msg),
   type: msgs.LiftState.typeName,
@@ -26,6 +32,12 @@ export const liftRequests: RomiTopic<msgs.LiftRequest> = {
   validate: msg => msgs.LiftRequest.fromObject(msg),
   type: msgs.LiftRequest.typeName,
   topic: 'lift_requests',
+};
+
+export const adapterLiftRequests: RomiTopic<msgs.LiftRequest> = {
+  validate: msg => msgs.LiftRequest.fromObject(msg),
+  type: msgs.LiftRequest.typeName,
+  topic: 'adapter_lift_requests',
 };
 
 export const stationRequest: RomiTopic<msgs.Station> = {
@@ -40,6 +52,12 @@ export const deliveryRequest: RomiTopic<msgs.Delivery> = {
   topic: 'delivery_requests',
 };
 
+export const loop: RomiTopic<msgs.Loop> = {
+  validate: msg => msgs.Loop.fromObject(msg),
+  type: msgs.Loop.typeName,
+  topic: 'loop_requests',
+};
+
 export const taskSummaries: RomiTopic<msgs.TaskSummary> = {
   validate: msg => msgs.TaskSummary.fromObject(msg),
   type: msgs.TaskSummary.typeName,
@@ -50,6 +68,36 @@ export const fleetStates: RomiTopic<msgs.FleetState> = {
   validate: msg => msgs.FleetState.fromObject(msg),
   type: msgs.FleetState.typeName,
   topic: 'fleet_states',
+};
+
+export const destinationRequests: RomiTopic<msgs.DestinationRequest> = {
+  validate: msg => msgs.DestinationRequest.fromObject(msg),
+  type: msgs.DestinationRequest.typeName,
+  topic: 'destination_requests',
+};
+
+export const robotModeRequests: RomiTopic<msgs.RobotMode> = {
+  validate: msg => msgs.RobotMode.fromObject(msg),
+  type: msgs.RobotMode.typeName,
+  topic: 'robot_mode_requests',
+};
+
+export const robotPathRequests: RomiTopic<msgs.PathRequest> = {
+  validate: msg => msgs.PathRequest.fromObject(msg),
+  type: msgs.PathRequest.typeName,
+  topic: 'robot_path_requests',
+};
+
+export const dispenserRequests: RomiTopic<msgs.DispenserRequest> = {
+  validate: msg => msgs.DispenserRequest.fromObject(msg),
+  type: msgs.DispenserRequest.typeName,
+  topic: 'dispenser_requests',
+};
+
+export const dispenserResults: RomiTopic<msgs.DispenserResult> = {
+  validate: msg => msgs.DispenserResult.fromObject(msg),
+  type: msgs.DispenserResult.typeName,
+  topic: 'dispenser_results',
 };
 
 export const dispenserStates: RomiTopic<msgs.DispenserState> = {
