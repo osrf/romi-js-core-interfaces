430dd77d632bad5796a1e18186f4a2da44714436
koonpeng <koonpeng@openrobotics.org>
2020-07-29 13:08:18 +0800

    refactor: move gentypes.py to bin dir

--- bin/generate_romi_msgs.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const ChildProcess = require('child_process');
-
-ChildProcess.spawn('python3', [
-  `${__dirname}/../scripts/gentypes.py`,
-  ...process.argv.slice(2),
-], { stdio: 'inherit' });
