480184061bbd09d86cc8a7da74c0509a351913d3
koonpeng <koonpeng@openrobotics.org>
2020-09-24 10:15:09 +0800

    update build step to work with ci

--- .github/actions/unit-test/unit-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-npm ci --unsafe-perm
-. /opt/ros/foxy/setup.bash && npm run build
-npm test
-CHROME_BIN=chromium-browser npm run test:browser -- --browsers ChromeHeadlessWithoutSandbox
