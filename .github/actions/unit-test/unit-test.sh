b984f21d79e64ceb5db051aa2c2a8d3a9a73b1fe
Teo Koon Peng <koonpeng@openrobotics.org>
2020-09-24 11:30:07 +0800

    build: update to foxy (#16)
    caveat: browser test is removed, this is due to ubuntu 20.04 chromium requiring snap, which doesn't plays well in docker. The current browser "test" is really just a smoke test anyway.

--- .github/actions/unit-test/unit-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-npm ci --unsafe-perm
-. /opt/ros/eloquent/setup.bash && npm run build
-npm test
-CHROME_BIN=chromium-browser npm run test:browser -- --browsers ChromeHeadlessWithoutSandbox
